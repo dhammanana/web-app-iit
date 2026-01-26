c2d24ad7a402635a9ac4b2ee6f82224351be5449
dhammanana <157960682+dhammanana@users.noreply.github.com>
2026-01-26 20:02:21 +0530

    changed map to IIT

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -38,13 +38,14 @@ export default function AboutUs() {
 
       <div className='mapWrapper'>
         {/* <iframe
+        https://www.google.com/maps/d/edit?mid=1yioZmMFnrYAp9vPNRYwvhnbwJJ201Fk&usp=sharing
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7902.62781765002!2d79.98147722617234!3d7.966475121082108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afd239053e24295%3A0x55e0440823643e03!2sIIT-%20International%20Institute%20of%20Therav%C4%81da!5e0!3m2!1sen!2slk!4v1751105066135!5m2!1sen!2slk"
           width="600" height="450" style="border:0;" allowFullScreen=""
           loading="lazy" referrerpolicy="no-referrer-when-downgrade">
 
         </iframe> */}
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7902.62781765002!2d79.98147722617234!3d7.966475121082108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afd239053e24295%3A0x55e0440823643e03!2sIIT-%20International%20Institute%20of%20Therav%C4%81da!5e0!3m2!1sen!2slk!4v1751105066135!5m2!1sen!2slk"
+          src="https://www.google.com/maps/d/embed?mid=1yioZmMFnrYAp9vPNRYwvhnbwJJ201Fk"
           width="600"
           height="450"
           style={{ border: 0 }}
